510bc4d6f3cc7744d01fd1f445f0abde66994250
chlendyd7 <chlendyd7@naver.com>
2025-03-04 20:31:15 +0900

    feat: admin 파일 추가 및 기타 커밋

--- lol/admin.py
+++ lol/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from lol.models import AdCarryChampion, Champion, Match, SupportChampion, Team, TeamComposition, TopChampion, JungleChampion, MidChampion
+
 # Register your models here.
+admin.site.register(Champion)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(TeamComposition)
+admin.site.register(TopChampion)
+admin.site.register(JungleChampion)
+admin.site.register(MidChampion)
+admin.site.register(AdCarryChampion)
+admin.site.register(SupportChampion)
