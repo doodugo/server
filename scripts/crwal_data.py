87ea9db4e59f2e1dacf8467ddd3f009a560f780d
chlendyd7 <chlendyd7@naver.com>
2025-03-04 20:30:10 +0900

    feat: crwaling data scripts 작성

--- scripts/crwal_data.py
+++ scripts/crwal_data.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+# //*[@id="mw-content-text"]/div/div[2]/div[6]/div/div/table/tbody/tr[1]/td[8]/span[1]
+# //*[@id="mw-content-text"]/div/div[2]/div[6]/div/div/table/tbody/tr[1]/td[8]/span[2]
+# //*[@id="mw-content-text"]/div/div[2]/div[6]/div/div/table/tbody/tr[2]/td[8]
+
 url = "https://lol.fandom.com/wiki/LCK/2025_Season/Cup/Match_History"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
---
+++ scripts/crwal_data5.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+
+url = "https://lol.fandom.com/wiki/LCK/2025_Season/Cup/Match_History"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+import os
+import sys
+import django
+from django.conf import settings
+
+# 프로젝트 루트 디렉토리를 Python 경로에 추가
+current_dir = os.path.dirname(os.path.abspath(__file__))
+server_dir = os.path.dirname(current_dir)
+sys.path.insert(0, server_dir)
+
+# Django 설정을 먼저 로드
+os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
+django.setup()
+
+# Django 설정이 로드된 후에 임포트
+from scripts.add_data import process_match_data, process_pick_data
+
+# 설정이 제대로 로드됐는지 확인 (선택사항)
+if not settings.configured:
+    raise RuntimeError("Django settings are not configured properly")
+
+# 특정 클래스를 가진 테이블의 tbody 선택
+table = soup.select_one('table.wikitable.hoverable-multirows.mhgame.sortable.plainlinks')
+if table:
+    rows = table.select('tbody tr')
+else:
+    rows = []
+
+print(len(rows))
+for row in rows[3:4]:
+    if row.get('class') == ['mhgame-blue']:
+        continue
+    # 날짜
+    date_td = row.select_one('td:nth-child(1)')
+    match, created, winner_check = process_match_data(row)
+
+    if created:
+        blue_champions, red_champions = process_pick_data(row, winner_check)
+
+    # TODO 플레이어 추가
+    # # 플레이어 (a 태그 텍스트 추출)
+    # blue_team_player = row.select_one('td:nth-child(10)')
+    # if blue_team_player:
+    #     player_links = blue_team_player.find_all('a', class_='catlink-players')
+    #     print("Blue Team Players:")
+    #     for link in player_links:
+    #         print(f"  {link.get_text()}")  # a 태그 안의 텍스트만 추출
+
+    # red_team_player = row.select_one('td:nth-child(11)')
+    # if red_team_player:
+    #     player_links = red_team_player.find_all('a', class_='catlink-players')
+    #     print("Red Team Players:")
+    #     for link in player_links:
+    #         print(f"  {link.get_text()}")  # a 태그 안의 텍스트만 추출
+
+print(f"Total matches: {len(rows)}")
