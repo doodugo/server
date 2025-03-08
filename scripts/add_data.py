a15e2262b0475e9e007f32da8b0acd61500a0d48
chlendyd7 <chlendyd7@naver.com>
2025-03-08 14:14:08 +0900

    feat: 팀 명 잘 들어오게 script 수정

--- scripts/add_data.py
+++ scripts/add_data.py
@@ -16,32 +16,44 @@ def create_or_get_champion_by_name(name):
 
 def process_match_data(row) -> tuple[Match, bool, str]:
     try:
-        print('이거 안됨?')
+        print('process_match_data 시작')
         date = datetime.strptime(
             row.select_one('td:nth-child(1)').get_text().strip(),
             '%Y-%m-%d'
         ).date()
         patch = row.select_one('td:nth-child(2)').get_text().strip()
-        blue_team_name = row.select_one('td:nth-child(3)').get_text().strip()
-        red_team_name = row.select_one('td:nth-child(4)').get_text().strip()
-        winner_name = row.select_one('td:nth-child(5)').get_text().strip()
-        print('이건 왜 안됨?:', blue_team_name, red_team_name, winner_name)
+        
+        blue_team_td = row.select_one('td:nth-child(3)')
+        blue_team_name = blue_team_td.select_one('a img').get('alt').replace('std', '').strip()
+        red_team_td = row.select_one('td:nth-child(4)')
+        red_team_name = red_team_td.select_one('a img').get('alt').replace('std', '').strip()
+        winner_td = row.select_one('td:nth-child(5)')
+        winner_name = winner_td.select_one('a img').get('alt').replace('std', '').strip()
+
+        print('팀 이름 디버깅:', blue_team_name, red_team_name, winner_name)
         blue_team, _ = Team.objects.get_or_create(
             name=blue_team_name,
         )
         red_team, _ = Team.objects.get_or_create(
             name=red_team_name,
         )
-        print(blue_team, red_team)
+        print('팀 디버깅:', blue_team, red_team)
         winner = blue_team if winner_name == blue_team_name else red_team
+        blue_td = row.select_one('td:nth-child(8)')
+        red_td = row.select_one('td:nth-child(9)')
+        winner_check = 'blue' if winner == blue_team else 'red'
+        blue_team_composition, red_team_composition = process_pick_data(blue_td, red_td, winner_check)
+
         match, created = Match.objects.get_or_create(
             date=date,
             patch=patch,
             blue_team=blue_team,
             red_team=red_team,
             winner=winner,
+            blue_composition=blue_team_composition,
+            red_composition=red_team_composition,
         )
-        winner_check = 'blue' if winner == blue_team else 'red'
+
         if created:
             print(f"Created match: {match}")
         else:
@@ -58,41 +70,36 @@ def process_match_data(row) -> tuple[Match, bool, str]:
         print(f"예상치 못한 오류 발생: {e}")
         raise
 
-def create_role_champions(champions):
+def create_role_champions(champions_obj):
     """포지션별 챔피언 객체 생성"""
     role_objects = []
     role_classes = [TopChampion, JungleChampion, MidChampion, AdCarryChampion, SupportChampion]
 
-    for champion, role_class in zip(champions, role_classes):
-        champ = create_or_get_champion_by_name(champion)
-        print(f"Creating {role_class.__name__} for {champ.name}")
-        role_obj, _ = role_class.objects.get_or_create(champion=champ)
+    for champion_obj, role_class in zip(champions_obj, role_classes):
+        print(f"Creating {role_class.__name__} for {champion_obj.name}")
+        role_obj, _ = role_class.objects.get_or_create(champion=champion_obj)
         role_objects.append(role_obj)
 
     return role_objects
 
-def process_pick_data(row, winner_check):
+def process_pick_data(blue_td, red_td, winner_check):
+    print('process_pick_data 시작')
     blue_champions = []
     red_champions = []
 
-    blue_td = row.select_one('td:nth-child(8)')
     if blue_td:
         blue_spans = blue_td.find_all('span', title=True)
-        print("Blue Team Champions:")
         for span in blue_spans:
-            champion = create_or_get_champion_by_name(span['title'])
-            blue_champions.append(champion)
+            champion_obj = create_or_get_champion_by_name(span['title'])
+            blue_champions.append(champion_obj)
 
-    red_td = row.select_one('td:nth-child(9)')
     if red_td:
         red_spans = red_td.find_all('span', title=True)
-        print("Red Team Champions:")
         for span in red_spans:
-            champion = create_or_get_champion_by_name(span['title'])
-            red_champions.append(champion)
+            champion_obj = create_or_get_champion_by_name(span['title'])
+            red_champions.append(champion_obj)
 
     if blue_champions and red_champions:
-        # 챔피언이 5명이 아닌 경우 에러 처리
         if len(blue_champions) != 5 or len(red_champions) != 5:
             print(f"Warning: Invalid team composition. Blue: {len(blue_champions)}, Red: {len(red_champions)}")
             assert False
@@ -109,7 +116,7 @@ def process_pick_data(row, winner_check):
             adc=blue_role_objects[3],
             support=blue_role_objects[4],
         )
-        
+
         red_team_composition, _ = TeamComposition.objects.get_or_create(
             top=red_role_objects[0],
             jungle=red_role_objects[1],
@@ -123,7 +130,7 @@ def process_pick_data(row, winner_check):
             pick_count=F('pick_count') + 1,
             win_count=F('win_count') + 1 if winner_check == 'blue' else F('win_count')
         )
-        
+
         TeamComposition.objects.filter(id=red_team_composition.id).update(
             pick_count=F('pick_count') + 1,
             win_count=F('win_count') + 1 if winner_check == 'red' else F('win_count')
