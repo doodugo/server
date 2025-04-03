f0a728e515da07cf0f6df9bae510adda031f9481
chlendyd7 <chlendyd7@naver.com>
2025-04-03 21:49:02 +0900

    feat: 데이터 url 추가

--- scripts/transform_map.py
+++ scripts/transform_map.py
@@ -17,7 +17,7 @@ transform_map = {
     "xin zhao": "xinzhao",
 }
 
-match_history_url = [
+LCK_URL = [
     "https://lol.fandom.com/wiki/LCK/2025_Season/Cup/Match_History",
     "https://lol.fandom.com/wiki/LCK/2024_Season/Summer_Season/Match_History",
     "https://lol.fandom.com/wiki/LCK/2024_Season/Spring_Season/Match_History",
@@ -25,8 +25,31 @@ match_history_url = [
     "https://lol.fandom.com/wiki/LCK/2024_Season/Summer_Season/Match_History",
     "https://lol.fandom.com/wiki/LCK/2024_Season/Summer_Playoffs/Match_History",
     "https://lol.fandom.com/wiki/LCK/2023_Season/Regional_Finals/Match_History",
+
+]
+
+LPL_URL = [
+    # split
+    'https://lol.fandom.com/wiki/LPL/2025_Season/Split_1/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2025_Season/Split_1_Playoffs/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2025_Season/Split_2_Placements/Match_History',
+
+    #2024
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Spring_Season/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Spring_Playoffs/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Summer_Placements/Match_Historyy',
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Summer_Season/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Summer_Playoffs/Match_History',
+    'https://lol.fandom.com/wiki/LPL/2024_Season/Regional_Finals/Match_History',
+
+]
+
+match_history_url = [
     'https://lol.fandom.com/wiki/2025_First_Stand/Match_History',
     'https://lol.fandom.com/wiki/2024_Mid-Season_Invitational/Match_History',
     'https://lol.fandom.com/wiki/2024_Mid-Season_Invitational/Match_History'
     'https://lol.fandom.com/wiki/2024_Season_World_Championship/Main_Event/Match_History',
+    
+    'https://lol.fandom.com/wiki/LCK_Academy_Series/2025_Season/1st_Championship/Match_History',
 ]
+match_history_url += LCK_URL + LPL_URL
