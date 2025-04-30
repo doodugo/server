a90acd51b79f246013981a8d343438ada9abebae
chlendyd7 <chlendyd7@naver.com>
2025-05-01 00:11:10 +0900

    feat: 챔피언 자동화 스크립트 완성

--- scripts/lol_api/ddragon_champion.py
+++ scripts/lol_api/ddragon_champion.py
@@ -1,19 +1,28 @@
 import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, timezone
+
+from setup_django import *
+from lol.models import PatchVersion, Champion
+
 
 def get_champion_information(version):
     url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/ko_KR/champion.json"
     response = requests.get(url)
     data = response.json()["data"]
-    for champion, info in data.items():
+    for info in data.values():
         champion_id = info["key"]
-        champion_name = info["name"]
+        champion_name = info["id"]
         full_image_url = f"https://ddragon.leagueoflegends.com/cdn/img/champion/loading/{champion_name}_0.jpg"
         square_image_url = f"https://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champion_name}.png"
+        Champion.objects.get_or_create(
+            id=champion_id,
+            name=champion_name,
+            name_ko=info["name"],
+            full_image_url=full_image_url,
+            icon_image_url=square_image_url,
+        )
         print(champion_id, champion_name)
 
 
 if __name__ == "__main__":
-    
-    get_champion_information()
+    version = PatchVersion.objects.order_by("-id").first()
+    get_champion_information(version.version)
