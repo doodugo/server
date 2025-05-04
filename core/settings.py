ec2cf1f100f72ebba9e3538c11c8d2eedac3a3e4
chlendyd7 <chlendyd7@naver.com>
2025-05-04 16:31:04 +0900

    feat: celery, redis 셋팅

--- core/settings.py
+++ core/settings.py
@@ -153,3 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Celery Setting
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
